98a6dfb115de95cbe76e84441da2923d413b961f
dursuninel <drsinl1905@gmail.com>
2024-09-25 23:46:08 +0300

    Lesson 5

--- script.js
+++ script.js
@@ -5,11 +5,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".square", {
-  x: 700,
+  //   x: 700,
   scrollTrigger: {
-    trigger: ".square",
-    start: "top 100%",
-    end: "top 0%",
+    trigger: ".square2",
+    start: "top 80%",
+    end: "top 20%",
+    pin: ".square",
+    pinSpacing: true,
     scrub: true,
     toggleActions: "restart pause resume reset ",
     markers: {
@@ -19,3 +21,60 @@ gsap.to(".square", {
     },
   },
 });
+
+gsap.to(".square2", {
+  //   x: 700,
+  scrollTrigger: {
+    trigger: ".square2",
+    start: "top 80%",
+    end: "top 20%",
+    pin: true,
+    pinSpacing: true,
+    scrub: true,
+    toggleActions: "restart pause resume reset ",
+    markers: {
+      startColor: "purple",
+      endColor: "fuchsia",
+      fontSize: "3rem",
+    },
+  },
+});
+
+
+gsap.to(".square3", {
+    //   x: 700,
+    scrollTrigger: {
+      trigger: ".square3",
+      start: "top 80%",
+      end: "top 20%",
+      pin: true,
+      pinSpacing: true,
+      scrub: true,
+      toggleActions: "restart pause resume reset ",
+      markers: {
+        startColor: "purple",
+        endColor: "fuchsia",
+        fontSize: "3rem",
+      },
+    },
+  });
+
+  
+  gsap.to(".square4", {
+    //   x: 700,
+    scrollTrigger: {
+      trigger: ".square4",
+      start: "top 80%",
+      end: "top 20%",
+      pin: true,
+      pinSpacing: true,
+      scrub: true,
+      toggleActions: "restart pause resume reset ",
+      markers: {
+        startColor: "purple",
+        endColor: "fuchsia",
+        fontSize: "3rem",
+      },
+    },
+  });
+  
